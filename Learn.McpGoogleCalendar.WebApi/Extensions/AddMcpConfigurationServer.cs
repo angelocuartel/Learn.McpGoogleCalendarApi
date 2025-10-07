031ebb4484a894d48be333c7aca45f3aedcba3c3
angelocuartel <angelocuartel@gmail.com>
2025-10-07 23:25:56 +0800

    feat: setup mcp server config

---
+++ Learn.McpGoogleCalendar.WebApi/Extensions/AddMcpConfigurationServer.cs
@@ -0,0 +1,15 @@
+﻿namespace Learn.McpGoogleCalendar.WebApi.Extensions
+{
+    public static partial class ConfigureExtenions
+    {
+        public static IServiceCollection AddMcpConfigurationServer
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMcpServer()
+            .WithStdioServerTransport()
+            .WithToolsFromAssembly();
+
+            return services;
+        }
+    }
+}
