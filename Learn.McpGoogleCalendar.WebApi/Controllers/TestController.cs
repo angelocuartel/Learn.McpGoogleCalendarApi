b4a1db7ec53bd0bb0e35a9b0dc2fa2a1fd99fb35
angelocuartel <angelocuartel@gmail.com>
2025-10-09 00:19:07 +0800

    feat: add test controller

---
+++ Learn.McpGoogleCalendar.WebApi/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace Learn.McpGoogleCalendar.WebApi.Controllers
+{
+    [McpServerToolType]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [McpServerTool, Description("Generate a list of testData.")]
+        [HttpGet]
+        public IActionResult GetTestData()
+        {
+            var testDataList = new List<TestData>
+            {
+                new TestData { Id = 1, Name = "Test One", CreatedAt = DateTime.UtcNow.AddDays(-2) },
+                new TestData { Id = 2, Name = "Test Two", CreatedAt = DateTime.UtcNow.AddDays(-1) },
+                new TestData { Id = 3, Name = "Test Three", CreatedAt = DateTime.UtcNow }
+            };
+
+            return Ok(testDataList);
+        }
+    }
+
+
+
+    public class TestData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
