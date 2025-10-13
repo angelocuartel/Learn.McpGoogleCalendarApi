4b56308403ad702e63c28840eff9fce177e79885
angelocuartel <angelocuartel@gmail.com>
2025-10-13 20:29:23 +0800

    chore: update insert event implementation

--- Learn.McpGoogleCalendar.WebApi/Controllers/CalendarController.cs
+++ Learn.McpGoogleCalendar.WebApi/Controllers/CalendarController.cs
@@ -13,9 +13,27 @@ public class CalendarController : ControllerBase
     public CalendarController(CalendarService calendarService)
     =>_calendarService = calendarService;
 
-    [HttpGet("events")]
-    public async Task<IActionResult> GetEvents(Event calendarEvent)
+    [HttpPost("events")]
+    public async Task<IActionResult> InsertEventAsync(Event calendarEvent)
     {
+        await _calendarService.Events.Insert(calendarEvent, "primary").ExecuteAsync();
+
+        return Ok();
+    }
+
+        [HttpPost("test/events")]
+    public async Task<IActionResult> InsertTestEventAsync()
+    {
+        var calendarEvent = new Event();
+        calendarEvent.Summary = "Test Event";
+        calendarEvent.Location = "800 Howard St., San Francisco, CA 94103";
+        calendarEvent.Description = "A chance to hear more about Google's developer products.";
+        calendarEvent.Start = new EventDateTime()
+        {
+            DateTimeDateTimeOffset = DateTime.Parse("2024-07-01T09:00:00-07:00"),
+            TimeZone = "America/Los_Angeles",
+        };
+
         await _calendarService.Events.Insert(calendarEvent, "primary").ExecuteAsync();
         
         return Ok();
--- Learn.McpGoogleCalendar.WebApi/Program.cs
+++ Learn.McpGoogleCalendar.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Google.Apis.Calendar.v3;
 using Learn.McpGoogleCalendar.WebApi.Extensions;
 
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<CalendarService>();
 
 builder.Services.AddMcpConfigurationServer(builder.Configuration);
 
